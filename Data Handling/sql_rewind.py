b970ab24f9d6b890f9a486df0fdda082d2fb08a7
EMK6025 <ethankcodes@gmail.com>
2025-05-13 23:15:05 -0400

    make functions modular

--- Data Handling/sql_rewind.py
+++ Data Handling/sql_rewind.py
@@ -1,8 +1,12 @@
 import pandas as pd
 from engine import connect
 
-engine = connect()
-df = pd.read_sql("SELECT * FROM data", engine)
-df.to_sql("test_data", engine, if_exists="replace", index=False)
+def main():
+    engine = connect()
+    df = pd.read_sql("SELECT * FROM data", engine)
+    df.to_sql("test_data", engine, if_exists="replace", index=False)
 
-print("test_data entries have been reset.\n")
+    print("test_data entries have been reset.\n")
+
+if __name__ == "__main__":
+    main()
