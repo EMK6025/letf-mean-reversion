e3526a5c9a2a7385adfdda89d8337f233f9642f6
EMK6025 <ethankcodes@gmail.com>
2025-05-14 00:08:26 -0400

    add SPX and exponential weighting RSI

--- Data Handling/load_SPX.py
+++ Data Handling/load_SPX.py
@@ -6,17 +6,18 @@ def main():
     ## Connect to SQL
     engine = connect()
 
-    df = pd.read_csv('SPXTR-data.csv', usecols=['Date', 'Close'])
+    df = pd.read_csv('SPX-data.csv', usecols=['Date', 'Close'])
 
     df["Date"] = pd.to_datetime(df["Date"], dayfirst=False).dt.date
     df['Close'] = df['Close'].astype(float)
 
     df = df.sort_values("Date")
-    df["SPXTR Change"] = df["Close"].pct_change() * 100
     
-    df = df.drop(columns="Close")
+    df = df.rename(columns={ # rename
+        "Close": "SPX Close"
+    })
     
-    update_sql_table(df, engine, "SPXTR")
+    update_sql_table(df, engine, "SPX")
 
 if __name__ == "__main__":
     main()
--- Data Handling/rsi.py
+++ Data Handling/rsi.py
@@ -3,15 +3,22 @@ from engine import connect
 import vectorbt as vbt
 from sql_functions import update_sql_table
 
-engine = connect()
-df = pd.read_sql("SELECT * FROM test_data", engine)
-df.set_index("Date", inplace=True)
+def main():
+    engine = connect()
+    df = pd.read_sql("SELECT * FROM test_data", engine)
+    df.set_index("Date", inplace=True)
+    df.sort_index(inplace=True)
+    series = pd.to_numeric(df["SPX Close"], errors="coerce").astype(float)
 
-for i in range(3,21):
-    rsi = vbt.RSI.run(df["SPXTR Change"], window=i)
-    df[f"{i} Day RSI"] = rsi.rsi
-    print(f"Finished processing {i} Day RSI\n")
+    for i in range(3,21):
+        rsi = vbt.RSI.run(series, window=i, ewm=True)
+        df[f"{i} Day RSI"] = rsi.rsi
+        print(f"Finished processing {i} Day RSI\n")
 
-with pd.option_context('display.max_columns', None):
-    view = df.head(20)
-    print(view)
+    with pd.option_context('display.max_columns', None):
+        view = df.tail(20)
+        print(view)
+
+    
+if __name__ == "__main__":
+    main()
