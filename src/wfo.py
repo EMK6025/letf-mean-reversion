92ebe313634baffc37963a1c64a8888569977609
Ethan Kuo <ethankuocodes@gmail.com>
2025-06-12 23:07:51 +0800

    wfo

--- src/wfo.py
+++ src/wfo.py
@@ -1,4 +1,13 @@
 from go import run_initial_population, create_next_generation
+from backtest import Portfolio
 
 # Generate and evaluate 500 random strategies
-population = run_initial_population(pop_size=500)
+# population = run_initial_population(pop_size=500)
+
+
+pf = Portfolio.from_orders(
+        close      = letf,
+        size       = size_changes,
+        size_type  = 'targetpercent',
+        freq       = '1D'
+    )
