b6c9465142dab41d6c590e16df94a5eede1ed849
Ethan Kuo <ethankuocodes@gmail.com>
2025-06-14 17:53:20 +0800

    edit gitignore

--- src/engine.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine as _sa_create_engine
-
-import pandas as pd
-
-def create_engine():
-    return _sa_create_engine("postgresql+psycopg://postgres:Mhkuu1.1213704313@localhost:5432/letf_data")
-
-def connect(engine, table_name = "test_data"):
-    return pd.read_sql(f"SELECT * FROM {table_name}", engine)
