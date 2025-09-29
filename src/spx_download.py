eecfbb5ecfa59bcccdc47050c638e1aa509882e8
Ethan Kuo <ethankuocodes@gmail.com>
2025-09-28 21:28:58 -0400

    merge etf data downloding scripts

--- src/spx_download.py
+++
@@ -1,8 +0,0 @@
-import yfinance as yf
-
-ticker = '^SPX'
-
-# Download historical data (adjust the start/end dates as needed)
-data = yf.download(ticker, start='1989-09-11', end='2025-05-12')
-
-data.to_csv('SPX-data.csv')
--- src/spxtr_download.py
+++
@@ -1,9 +0,0 @@
-import yfinance as yf
-
-# Choose your ticker, e.g., SPXTR (S&P 500 Total Return Index)
-ticker = '^SP500TR'  # Use '^' for indices on Yahoo
-
-# Download historical data (adjust the start/end dates as needed)
-data = yf.download(ticker, start='1989-09-11', end='2025-05-12')
-
-data.to_csv('SPXTR-data.csv')
