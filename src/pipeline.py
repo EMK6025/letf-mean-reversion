8cf1a866bc11bf42c3e906a3d5a721918e577a2f
Ethan Kuo <ethankuocodes@gmail.com>
2025-11-11 17:33:38 -0500

    update manual.py to pipeline.py, easier to use

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,4 +1,5 @@
 from typing import List
+import random
 
 def wfo():
     from wfo import walk_forward_optimization
@@ -7,72 +8,38 @@ def wfo():
     start_date = '1990-01-01'
     end_date = '2024-12-31'
     
+    # set random seed
+    random.seed(2025)
+    
     custom_config = FitnessConfig(
         selected_metrics=['sortino', 'drawdown', 'annual_return', 'var'],
         enable_bottom_percentile_filter=True,
         bottom_percentile=10.0
     )
     
-    try:
-        walk_forward_optimization(
-            start_date=start_date,
-            end_date=end_date,  
-            in_sample_months=60,
-            out_sample_months=6,
-            max_time_minutes=1000,
-            stall_generations=10,
-            max_generations=1000,
-            pop_size=1000,
-            n_ensemble=50,
-            leverage=3,
-            fitness_config=custom_config,
-            rand_seed = 1013
-        )
-    except Exception as e:
-        print('Something when wrong, go fix.')
-        traceback.print_exc()
-    
-def PCA(run_index):
-    from backtest_analysis import analyze_PCA
-    analyze_PCA(run_index)
-
-def run(run_id):
-    from backtest_analysis import analyze_wfo
-    analyze_wfo(run_id)
-
-def fit():
-    from backtest_analysis import analyze_fit
-    analyze_fit()
-    
+    for _ in range(0,25):
+        try:
+            walk_forward_optimization(
+                start_date=start_date,
+                end_date=end_date,  
+                in_sample_months=60,
+                out_sample_months=6,
+                max_time_minutes=1000,
+                stall_generations=10,
+                max_generations=1000,
+                pop_size=1000,
+                n_ensemble=50,
+                leverage=3,
+                fitness_config=custom_config,
+                rand_seed = random.randint(1, 10000)
+            )
+        except Exception as e:
+            print('Something went wrong, go fix.')
+            traceback.print_exc()
+      
 def grab_runs():
     from backtest_analysis import list_run_ids
     return list_run_ids().tolist()
-
-def list_runs():
-    from backtest_analysis import list_run_ids
-    runs = list_run_ids()
-    print(run)
-    return list_run_ids().tolist()
-    
-def reset_wfo():
-    from engine import create_engine
-    from wfo_sql import reset
-    engine = create_engine()
-    reset(engine)
-    
-def clear_runs(to_remove: List):
-    from engine import create_engine
-    from wfo_sql import clear
-    engine = create_engine()
-    clear(engine, to_remove)
-    
-def alpha_all():
-    from backtest_analysis import analyze_alpha_all
-    analyze_alpha_all()
-
-def alpha(run_ids):
-    from backtest_analysis import analyze_probability_of_outperformance
-    analyze_probability_of_outperformance(run_ids)
 
 def graph_base_holdings(run_ids):
     from backtest_analysis import analyse_gameplan
@@ -118,4 +85,4 @@ def graph_performance(run_ids):
     plt.show()
     
 if __name__ == '__main__':
-    list_runs()
+    wfo()
